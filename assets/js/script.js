a2fa115b11f02bda80a52ed7ff175c5436b8c692
Megan Ellman <meganlellman@gmail.com>
2024-12-12 13:08:55 -0800

    added play again button

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,43 +2,39 @@ var startBtn = document.querySelector('#start-btn');
 
 var wordBank = ['river', 'mountain', 'sunshine', 'whisper', 'galaxy', 'sparkle', 'echo', 'twilight', 'breeze', 'harmony', 'adventure', 'serenity', 'journey', 'mystery', 'enigma', 'essence', 'fusion', 'glimmer', 'wonder', 'tranquility', 'radiance', 'cascade', 'meadow', 'forest', 'horizon', 'illusion', 'crystal', 'ripple', 'whirlwind', 'ember', 'solstice', 'mirage', 'glow', 'whimsy', 'enchantment', 'voyage', 'odyssey', 'reverie', 'miracle', 'dawn', 'tempest', 'serendipity', 'fantasy', 'infinity', 'labyrinth', 'paradise', 'zephyr', 'symphony', 'echoes', 'aurora', 'starlight', 'ebony', 'sapphire', 'emerald', 'nectar', 'tapestry', 'illusion', 'epiphany', 'marvel', 'revelation', 'ephemeral', 'charm', 'whispering', 'luminescence', 'melody', 'journey', 'elegance', 'grace', 'allure', 'mystique', 'captivate', 'poise', 'splendor', 'magnificence', 'enigma', 'tranquil', 'celestial', 'ethereal', 'arcane', 'vortex', 'kaleidoscope', 'quintessential', 'halcyon', 'luminous', 'inspiration', 'profound', 'whisper', 'radiant', 'resonance', 'wonder', 'solitude', 'panorama', 'harmony', 'timeless', 'infinitesimal'];
 
-startBtn.addEventListener('click', function(){
-    if(document.querySelector('#time-up-msg')){
+startBtn.addEventListener('click', playGame);
 
-        var timeUp = document.querySelector('#time-up-msg');
-        if(timeUp.style.visibility = 'visible'){
-           timeUp.style.visibility = 'hidden'
-        }
-    }
-    startBtn.style.visibility = 'hidden';
-
-    var gameContainer = document.createElement('div');
-    document.body.append(gameContainer);
-    gameContainer.setAttribute('id', 'game-container')
-
-    var displayContainer = document.createElement('div');
-    displayContainer.setAttribute('id', 'display-container');
-    gameContainer.append(displayContainer);
+function playGame(){
+        setInterval(timeCount, 1000);
+        startBtn.style.display = 'none';
     
-    var letterBank = document.createElement('div');
-    letterBank.setAttribute('id', 'letter-bank')
-    gameContainer.append(letterBank);
-
-    var wordAnswer = generateWord();
-    for(var i = 0; i < wordAnswer.length; i++){
-        var wordDisplay = document.createElement('h1');
-        wordDisplay.textContent = wordAnswer[i].toUpperCase();
-
-        var letterDiv = document.createElement('div');
-        letterDiv.append(wordDisplay);
-        displayContainer.append(letterDiv);
+        var gameContainer = document.createElement('div');
+        document.body.append(gameContainer);
+        gameContainer.setAttribute('id', 'game-container')
+    
+        var displayContainer = document.createElement('div');
+        displayContainer.setAttribute('id', 'display-container');
+        gameContainer.append(displayContainer);
         
-        wordDisplay.setAttribute('class', 'word-display');
-        letterDiv.setAttribute('class', 'letter-div');
-
-    }
-    generateLetterBox(letterBank)
-})
+        var letterBank = document.createElement('div');
+        letterBank.setAttribute('id', 'letter-bank')
+        gameContainer.append(letterBank);
+    
+        var wordAnswer = generateWord();
+        for(var i = 0; i < wordAnswer.length; i++){
+            var wordDisplay = document.createElement('h1');
+            wordDisplay.textContent = wordAnswer[i].toUpperCase();
+    
+            var letterDiv = document.createElement('div');
+            letterDiv.append(wordDisplay);
+            displayContainer.append(letterDiv);
+            
+            wordDisplay.setAttribute('class', 'word-display');
+            letterDiv.setAttribute('class', 'letter-div');
+    
+        }
+        generateLetterBox(letterBank);
+};
 
 function generateWord(){
     var word = wordBank[Math.floor(Math.random() * 100)];
@@ -72,22 +68,40 @@ function generateLetterBox(letterBank){
 }
 
 var timer = document.querySelector('#timer');
-var time = 2;
+var time = 3;
 
-setInterval(() => {
+function timeCount() {
+    console.log('timeCount');
     time--;
     timer.textContent = `${time}s`;
     if(time === 0){
-        timer.style.display = 'none';
+        timer.style.visibility = 'hidden';
         document.querySelector('#game-container').style.display = 'none';
+        
+        document.querySelector('#game-container').remove();
 
         var gameOver = document.createElement('h1');
         gameOver.textContent = 'Times up! Play Again?';
         gameOver.setAttribute('id', 'time-up-msg');
         
-       startBtn.textContent = 'Play Again';
-       startBtn.style.visibility = 'visible';
+        var playAgainBtn = document.createElement('button');
+        playAgainBtn.setAttribute('class', 'game-btn');
+        
+        playAgainBtn.addEventListener('click', function(){
+            var timeUp = document.querySelector('#time-up-msg');
+            timeUp.remove();
+            time = 6;
+            if(time = 5){
+                timer.style.visibility = 'visible'
+            };
+            playAgainBtn.style.display = 'none';
+
+            playGame();
+        })
+
+        playAgainBtn.textContent = 'Play Again?';
+        document.body.append(playAgainBtn);
         document.body.append(gameOver)
         clearInterval();
     }
-}, 1000)
+};
